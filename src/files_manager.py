249c2c9c8b146f3b3713f206d7b1703e6a6201a7
frankmurrey <sekachgamer@gmail.com>
2023-05-26 18:16:47 +0500

    ADD warmup and wallet log funcs

--- src/files_manager.py
+++ src/files_manager.py
@@ -1,6 +1,8 @@
 import yaml
+import os
+import json
 
-from src.paths import CONFIG_FILE, WALLETS_FILE, APTOS_WALLETS_FILE
+from src.paths import CONFIG_FILE, WALLETS_FILE, APTOS_WALLETS_FILE, WARM_UP_CONFIG_FILE, WALLET_LOGS_DIR, MAIN_DIR
 from loguru import logger
 
 
@@ -21,7 +23,6 @@ def read_aptos_wallets_from_file():
         with open(APTOS_WALLETS_FILE, "r") as file:
             wallets_from_txt = file.read().splitlines()
             if wallets_from_txt is None:
-                print("No wallets found")
                 return []
             return [x for x in wallets_from_txt if x and x.strip() and len(x) == 66]
     except FileNotFoundError:
@@ -40,3 +41,38 @@ def load_config():
             return yaml.load(f, Loader=yaml.FullLoader)
     except FileNotFoundError:
         logger.error("Config file not found")
+
+
+def load_warmup_config():
+    try:
+        with open(WARM_UP_CONFIG_FILE, "r") as f:
+            return yaml.load(f, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        logger.error("Warmup Config file not found")
+
+
+def get_all_wallets_logs_data():
+    files_dir = WALLET_LOGS_DIR
+    all_data = []
+
+    for filename in os.listdir(files_dir):
+        if filename.endswith(".json"):
+            # Open the file and load the data
+            with open(os.path.join(files_dir, filename)) as file:
+                file_data = json.load(file)
+            # Add the data to the list
+            all_data.append(file_data)
+
+    combined_data = []
+    for data in all_data:
+        combined_data.append(data)
+
+    return combined_data
+
+
+def save_summary_log_file():
+    data = get_all_wallets_logs_data()
+    path = os.path.join(MAIN_DIR, 'summary_logs.json')
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+        logger.info(f'Summary log file created in local directory\n')
