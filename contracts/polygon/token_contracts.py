27b34f24aede3f37ec3fd8236a04783104c3a08f
frankmurrey <sekachgamer@gmail.com>
2023-06-05 17:08:35 +0500

    ADD coin STG

--- contracts/polygon/token_contracts.py
+++ contracts/polygon/token_contracts.py
@@ -9,9 +9,12 @@ with open(PolygonDir.USDT_ABI_FILE, "r") as file:
 with open(PolygonDir.USDC_ABI_FILE, "r") as file:
     USDC_ABI = json.load(file)
 
+with open(PolygonDir.STG_ABI_FILE, "r") as file:
+    STG_ABI = json.load(file)
+
 
 polygon_tokens = [
     Token(name="USDC", abi=USDC_ABI, address="0x2791bca1f2de4661ed88a30c99a7a9449aa84174", pool_id=1),
-    Token(name="USDT", abi=USDT_ABI, address="0xc2132D05D31c914a87C6611C10748AEb04B58e8F", pool_id=2)
+    Token(name="USDT", abi=USDT_ABI, address="0xc2132D05D31c914a87C6611C10748AEb04B58e8F", pool_id=2),
+    Token(name="STG", abi=STG_ABI, address="0x2f6f07cdcf3588944bf4c42ac74ff24bf56e7590", pool_id=None)
 ]
-
