7f0fd518cf69a85f4334f9e95fac90dcd2988e56
frankmurrey <sekachgamer@gmail.com>
2023-06-07 00:32:09 +0500

    ADD rpcs.json autocreate if FileNotFound

--- contracts/rpcs.py
+++ contracts/rpcs.py
@@ -1,9 +1,16 @@
 import json
 
 from src.paths import RPCS_FILE
+from src.templates import create_rpcs_file
 
-with open(RPCS_FILE, "r") as f:
-    RPC = json.load(f)
+try:
+    with open(RPCS_FILE, "r") as f:
+        RPC = json.load(f)
+except FileNotFoundError:
+    create_rpcs_file()
+    with open(RPCS_FILE, "r") as f:
+        RPC = json.load(f)
+
 
 # Please change rpc URLS there →: contracts\rpcs.json
 # Please change rpc URLS there →: contracts\rpcs.json
