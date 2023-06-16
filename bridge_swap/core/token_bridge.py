f3fce9e982904dfe01c0c3988954b0705bc52a91
frankmurrey <sekachgamer@gmail.com>
2023-06-16 15:07:31 +0500

    ADD wait for txn receipt, UPD log texts

--- bridge_swap/core/token_bridge.py
+++ bridge_swap/core/token_bridge.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from bridge_swap.base_bridge import BridgeBase
 from src.files_manager import read_evm_wallets_from_file
 from src.schemas.config import ConfigSchema
-from src.config import get_config, print_config
+from src.config import print_config
 from src.rpc_manager import RpcValidator
 
 from loguru import logger
@@ -23,11 +23,15 @@ def core_mass_transfer(config_data: ConfigSchema):
     wallet_number = 1
     wallets_amount = len(wallets)
     for wallet in wallets:
+        blured_wallet = token_bridge.blur_private_key(private_key=wallet)
+        wallet_address = token_bridge.get_wallet_address(private_key=wallet)
+        logger.info(f"[{wallet_number}] - {wallet_address}")
+        logger.info(f"Got wallet pk /{blured_wallet}/")
 
         if config_data.source_chain.lower() == "bsc":
-            bridge_status = token_bridge.transfer(private_key=wallet, wallet_number=wallet_number)
+            bridge_status = token_bridge.transfer(private_key=wallet)
         else:
-            bridge_status = token_bridge.transfer_from_core(private_key=wallet, wallet_number=wallet_number)
+            bridge_status = token_bridge.transfer_from_core(private_key=wallet)
 
         if wallet_number == wallets_amount:
             logger.info(f"Bridge process is finished\n")
@@ -64,7 +68,7 @@ class CoreDaoBridger(BridgeBase):
             logger.error(f"Bridge of {self.config_data.source_coin_to_transfer} is not supported between"
                          f" {self.config_data.source_chain} and {self.config_data.target_chain}")
 
-    def transfer(self, private_key, wallet_number):
+    def transfer(self, private_key, wallet_number=None):
         if not self.token_obj:
             return
 
@@ -72,7 +76,7 @@ class CoreDaoBridger(BridgeBase):
         wallet_address = self.get_wallet_address(private_key=private_key)
         wallet_token_balance_wei = self.get_token_balance(wallet_address=source_wallet_address,
                                                           token_contract=self.token_contract)
-        wallet_token_balance = wallet_token_balance_wei / 10 ** self.get_token_decimals(self.token_contract)
+        wallet_token_balance_decimals = wallet_token_balance_wei / 10 ** self.get_token_decimals(self.token_contract)
 
         if self.config_data.send_to_one_address is True:
             dst_wallet_address = self.get_checksum_address(self.config_data.address_to_send)
@@ -80,36 +84,33 @@ class CoreDaoBridger(BridgeBase):
             dst_wallet_address = wallet_address
 
         if self.config_data.send_all_balance is True:
-            token_amount_out = wallet_token_balance_wei
-            if token_amount_out == 0:
-                logger.error(f"{wallet_number} [{source_wallet_address}] - {self.config_data.source_coin_to_transfer} "
+            token_amount_out_wei = wallet_token_balance_wei
+            if token_amount_out_wei == 0:
+                logger.error(f"{self.config_data.source_coin_to_transfer} "
                              f"({self.config_data.source_chain}) balance is 0")
                 return
         else:
-            token_amount_out = self.get_random_amount_out(min_amount=self.min_bridge_amount,
+            token_amount_out_wei = self.get_random_amount_out(min_amount=self.min_bridge_amount,
                                                           max_amount=self.max_bridge_amount,
                                                           token_contract=self.token_contract)
 
-        wallet_number = self.get_wallet_number(wallet_number=wallet_number)
+        token_amount_out_decimals = token_amount_out_wei / 10 ** self.get_token_decimals(self.token_contract)
 
-        if wallet_token_balance_wei < token_amount_out:
-            logger.error(f"{wallet_number} [{source_wallet_address}] - {self.config_data.source_coin_to_transfer} "
-                         f"({self.config_data.source_chain})"
-                         f" balance not enough "
-                         f"to bridge. Balance: {wallet_token_balance}")
+        if wallet_token_balance_wei < token_amount_out_wei:
+            logger.error(f"{self.config_data.source_coin_to_transfer} ({self.config_data.source_chain})"
+                         f" balance not enough to bridge. Balance: {wallet_token_balance_decimals}")
             return
 
         allowed_amount_to_bridge = self.check_allowance(wallet_address=wallet_address,
                                                         token_contract=self.token_contract,
                                                         spender=self.source_chain.core_dao_router_address)
 
-        if allowed_amount_to_bridge < token_amount_out:
+        if allowed_amount_to_bridge < token_amount_out_wei:
             logger.warning(
-                f"{wallet_number} [{source_wallet_address}] - Not enough allowance for {self.token_obj.name},"
-                f" approving {self.token_obj.name} to bridge")
+                f"Not enough allowance for {self.token_obj.name}, approving {self.token_obj.name} to bridge")
 
             token_approval = self.make_approve_for_token(private_key=private_key,
-                                                         target_approve_amount=token_amount_out,
+                                                         target_approve_amount=token_amount_out_wei,
                                                          token_contract=self.token_contract,
                                                          token_obj=self.token_obj,
                                                          spender=self.source_chain.core_dao_router_address)
@@ -117,10 +118,10 @@ class CoreDaoBridger(BridgeBase):
             if token_approval is not True:
                 return
         else:
-            logger.info(f"{wallet_number} [{source_wallet_address}] - Wallet has enough allowance to bridge")
+            logger.info(f"Wallet has enough allowance to bridge")
 
         txn = self.build_token_bridge_core_tx(wallet_address=wallet_address,
-                                              amount_out=token_amount_out,
+                                              amount_out=token_amount_out_wei,
                                               token_obj=self.token_obj,
                                               dst_wallet_address=dst_wallet_address)
 
@@ -131,21 +132,35 @@ class CoreDaoBridger(BridgeBase):
                 txn['gas'] = estimated_gas_limit
 
             if self.config_data.test_mode is True:
-                logger.info(f"{wallet_number} [{source_wallet_address}] - Estimated gas limit for "
-                            f"{self.config_data.source_chain} → {self.config_data.target_chain} "
-                            f"{self.token_obj.name} bridge: {estimated_gas_limit}")
+                logger.info(f"Estimated gas limit for {self.config_data.source_chain} → {self.config_data.target_chain}"
+                            f" {self.token_obj.name} bridge: {estimated_gas_limit}")
                 return
 
             signed_txn = self.web3.eth.account.sign_transaction(txn, private_key=private_key)
             tx_hash = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-            logger.success(f"{wallet_number} [{source_wallet_address}] - Transaction sent: {tx_hash.hex()}")
 
-            return tx_hash.hex()
+            if self.config_data.wait_for_confirmation is True:
+                time_out = self.config_data.confirmation_timeout_seconds
+                tx_receipt = self.wait_for_tx_receipt(tx_hash=tx_hash, time_out=time_out)
+                if tx_receipt['status'] == 1:
+                    logger.success(
+                        f"{self.config_data.source_chain} → {self.config_data.target_chain}"
+                        f" ({token_amount_out_decimals} {self.token_obj.name})"
+                        f" bridge transaction success: {tx_hash.hex()}")
+                    return tx_hash.hex()
+                else:
+                    logger.error(f"Transaction is not success: {tx_hash.hex()}")
+
+            else:
+                logger.success(
+                    f"[{self.config_data.source_chain} → {self.config_data.target_chain} "
+                    f"({token_amount_out_decimals} {self.token_obj.name}) bridge transaction sent: {tx_hash.hex()}")
+                return tx_hash.hex()
         except Exception as e:
-            logger.error(f"{wallet_number} [{source_wallet_address}] - Error while sending  transaction: {e}")
+            logger.error(f"Error while sending  transaction: {e}")
             return
 
-    def transfer_from_core(self, private_key, wallet_number):
+    def transfer_from_core(self, private_key, wallet_number=None):
         if not self.token_obj:
             return
 
@@ -153,44 +168,43 @@ class CoreDaoBridger(BridgeBase):
         wallet_address = self.get_wallet_address(private_key=private_key)
         wallet_token_balance_wei = self.get_token_balance(wallet_address=source_wallet_address,
                                                           token_contract=self.token_contract)
-        wallet_token_balance = wallet_token_balance_wei / 10 ** self.get_token_decimals(self.token_contract)
+        wallet_token_balance_decimals = wallet_token_balance_wei / 10 ** self.get_token_decimals(self.token_contract)
 
         if self.config_data.send_to_one_address is True:
             dst_wallet_address = self.get_checksum_address(self.config_data.address_to_send)
         else:
             dst_wallet_address = wallet_address
 
-        wallet_number = self.get_wallet_number(wallet_number=wallet_number)
-
         if self.config_data.send_all_balance is True:
-            token_amount_out = wallet_token_balance_wei
-            if token_amount_out == 0:
-                logger.error(f"{wallet_number} [{source_wallet_address}] - {self.config_data.source_coin_to_transfer} "
+            token_amount_out_wei = wallet_token_balance_wei
+            if token_amount_out_wei == 0:
+                logger.error(f"{self.config_data.source_coin_to_transfer} "
                              f"({self.config_data.source_chain}) balance is 0")
                 return
         else:
-            token_amount_out = self.get_random_amount_out(min_amount=self.min_bridge_amount,
-                                                          max_amount=self.max_bridge_amount,
-                                                          token_contract=self.token_contract)
+            token_amount_out_wei = self.get_random_amount_out(min_amount=self.min_bridge_amount,
+                                                              max_amount=self.max_bridge_amount,
+                                                              token_contract=self.token_contract)
 
-        if wallet_token_balance_wei < token_amount_out:
-            logger.error(f"{wallet_number} [{source_wallet_address}] - {self.config_data.source_coin_to_transfer} "
+        token_amount_out_decimals = token_amount_out_wei / 10 ** self.get_token_decimals(self.token_contract)
+
+        if wallet_token_balance_wei < token_amount_out_wei:
+            logger.error(f"{self.config_data.source_coin_to_transfer} "
                          f"({self.config_data.source_chain})"
                          f" balance not enough "
-                         f"to bridge. Balance: {wallet_token_balance}")
+                         f"to bridge. Balance: {wallet_token_balance_decimals}")
             return
 
         allowed_amount_to_bridge = self.check_allowance(wallet_address=wallet_address,
                                                         token_contract=self.token_contract,
                                                         spender=self.source_chain.router_address)
 
-        if allowed_amount_to_bridge < token_amount_out:
+        if allowed_amount_to_bridge < token_amount_out_wei:
             logger.warning(
-                f"{wallet_number} [{source_wallet_address}] - Not enough allowance for {self.token_obj.name},"
-                f" approving {self.token_obj.name} to bridge")
+                f"Not enough allowance for {self.token_obj.name}, approving {self.token_obj.name} to bridge")
 
             token_approval = self.make_approve_for_token(private_key=private_key,
-                                                         target_approve_amount=token_amount_out,
+                                                         target_approve_amount=token_amount_out_wei,
                                                          token_contract=self.token_contract,
                                                          token_obj=self.token_obj,
                                                          spender=self.source_chain.router_address)
@@ -198,10 +212,10 @@ class CoreDaoBridger(BridgeBase):
             if token_approval is not True:
                 return
         else:
-            logger.info(f"{wallet_number} [{source_wallet_address}] - Wallet has enough allowance to bridge")
+            logger.info(f"Wallet has enough allowance to bridge")
 
         txn = self.build_token_bridge_frome_core_tx(wallet_address=wallet_address,
-                                                    amount_out=token_amount_out,
+                                                    amount_out=token_amount_out_wei,
                                                     token_obj=self.token_obj,
                                                     dst_wallet_address=dst_wallet_address)\
 
@@ -212,17 +226,32 @@ class CoreDaoBridger(BridgeBase):
                 txn['gas'] = estimated_gas_limit
 
             if self.config_data.test_mode is True:
-                logger.info(f"{wallet_number} [{source_wallet_address}] - Estimated gas limit for "
+                logger.info(f"Estimated gas limit for "
                             f"{self.config_data.source_chain} → {self.config_data.target_chain} "
                             f"{self.token_obj.name} bridge: {estimated_gas_limit}")
                 return
 
             signed_txn = self.web3.eth.account.sign_transaction(txn, private_key=private_key)
             tx_hash = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-            logger.success(f"{wallet_number} [{source_wallet_address}] - Transaction sent: {tx_hash.hex()}")
 
-            return tx_hash.hex()
+            if self.config_data.wait_for_confirmation is True:
+                time_out = self.config_data.confirmation_timeout_seconds
+                tx_receipt = self.wait_for_tx_receipt(tx_hash=tx_hash, time_out=time_out)
+                if tx_receipt['status'] == 1:
+                    logger.success(
+                        f"{self.config_data.source_chain} → {self.config_data.target_chain}"
+                        f" ({token_amount_out_decimals} {self.token_obj.name})"
+                        f" bridge transaction success: {tx_hash.hex()}")
+                    return tx_hash.hex()
+                else:
+                    logger.error(f"Transaction is not success: {tx_hash.hex()}")
+
+            else:
+                logger.success(
+                    f"{self.config_data.source_chain} → {self.config_data.target_chain}"
+                    f" ({token_amount_out_decimals} {self.token_obj.name}) bridge transaction sent: {tx_hash.hex()}")
+                return tx_hash.hex()
         except Exception as e:
-            logger.error(f"{wallet_number} [{source_wallet_address}] - Error while sending  transaction: {e}")
+            logger.error(f"Error while sending  transaction: {e}")
             return
 
